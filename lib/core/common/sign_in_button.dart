b43d037496ed6f50b1303dcf018d5d0385bb87a7
manoharacharyaa <manoharacharya63@gmail.com>
2025-06-14 16:00:55 +0530

    3. google signin with repository and controller

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/theme/pallet.dart';
 
-class SignInButton extends StatelessWidget {
+class SignInButton extends ConsumerWidget {
   const SignInButton({super.key});
 
+  void signinWithGoogle(WidgetRef ref) {
+    ref.read(authControllerProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: ElevatedButton.icon(
-        onPressed: () {},
+        onPressed: () => signinWithGoogle(ref),
         label: const Text(
           'Continue with Google',
           style: TextStyle(color: Colors.white),
