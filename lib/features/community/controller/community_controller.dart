826e49e6d5684caaf92b3bac4f43a87c9a607cb0
manoharacharyaa <manoharacharya63@gmail.com>
2025-06-29 16:57:49 +0530

    9. community screen

--- lib/features/community/controller/community_controller.dart
+++ lib/features/community/controller/community_controller.dart
@@ -21,6 +21,12 @@ final communityControllerProvider =
       );
     });
 
+final getCommunityByNameProvider = StreamProvider.family((ref, String name) {
+  return ref
+      .watch(communityControllerProvider.notifier)
+      .getCommunityByName(name);
+});
+
 class CommunityController extends StateNotifier<bool> {
   final CommunityRepository _communityRepository;
   final Ref _ref;
@@ -55,5 +61,9 @@ class CommunityController extends StateNotifier<bool> {
   Stream<List<Community>> getUserCommunities() {
     final uid = _ref.read(userProvider)!.uid;
     return _communityRepository.getUserCommunity(uid);
+  }
+
+  Stream<Community> getCommunityByName(String name) {
+    return _communityRepository.getCommunityByName(name);
   }
 }
---
+++ lib/features/community/screens/community_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
+
+class CommunityScreen extends ConsumerWidget {
+  final String name;
+
+  const CommunityScreen({
+    super.key,
+    required this.name,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider)!;
+    return Scaffold(
+      body: ref
+          .watch(getCommunityByNameProvider(name))
+          .when(
+            data: (community) => NestedScrollView(
+              headerSliverBuilder: (context, innerBoxIsScrolled) {
+                return [
+                  SliverAppBar(
+                    expandedHeight: 150,
+                    floating: true,
+                    snap: true,
+                    flexibleSpace: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: Image.network(
+                            community.banner,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(16),
+                    sliver: SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: CircleAvatar(
+                              backgroundImage: NetworkImage(community.avatar),
+                              radius: 35,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Text(
+                                'r/${community.name}',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const Spacer(),
+                              community.mods.contains(user.uid)
+                                  ? OutlinedButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        side: WidgetStatePropertyAll(
+                                          BorderSide(color: Colors.blueAccent),
+                                        ),
+                                        shape: WidgetStatePropertyAll(
+                                          RoundedSuperellipseBorder(
+                                            borderRadius:
+                                                BorderRadiusGeometry.circular(
+                                                  20,
+                                                ),
+                                          ),
+                                        ),
+                                        padding: WidgetStatePropertyAll(
+                                          const EdgeInsets.symmetric(
+                                            horizontal: 25,
+                                          ),
+                                        ),
+                                      ),
+                                      child: Text(
+                                        'Mod Tools',
+                                        style: TextStyle(
+                                          color: Colors.blueAccent,
+                                        ),
+                                      ),
+                                    )
+                                  : OutlinedButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        side: WidgetStatePropertyAll(
+                                          BorderSide(color: Colors.blueAccent),
+                                        ),
+                                        shape: WidgetStatePropertyAll(
+                                          RoundedSuperellipseBorder(
+                                            borderRadius:
+                                                BorderRadiusGeometry.circular(
+                                                  20,
+                                                ),
+                                          ),
+                                        ),
+                                        padding: WidgetStatePropertyAll(
+                                          const EdgeInsets.symmetric(
+                                            horizontal: 25,
+                                          ),
+                                        ),
+                                      ),
+                                      child: Text(
+                                        community.members.contains(user.uid)
+                                            ? 'Joined'
+                                            : 'Join',
+                                        style: TextStyle(
+                                          color: Colors.blueAccent,
+                                        ),
+                                      ),
+                                    ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text('${community.members.length} members'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ];
+              },
+              body: const Text('Displaying Posts'),
+            ),
+            error: (error, stackTrace) {},
+            loading: () => const Loader(),
+          ),
+    );
+  }
+}
